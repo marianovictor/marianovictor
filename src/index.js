1284b46f55462e7fe20a025e360ada6d9d486b75
Victor Rocha <Victor.rocha2002@gmail.com>
2022-01-05 20:46:45 -0500

    Criando a base(pasta com rotas, iniciando na main)

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Main from "./pages/Main";
+
+import Routes from './routes';
+
+export default function App(){
+    return(
+        <Routes/>
+    );
+}
---
+++ src/pages/Main.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+
+export default function Main(){
+    return <View>
+        <Text>Ola galerinha do youtube</Text>
+    </View>
+}
